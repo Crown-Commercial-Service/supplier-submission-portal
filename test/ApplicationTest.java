4b45e491c10289049445456d07b2cf9d0fa193e3
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2014-09-15 15:14:27 +0100

    Fix broken test

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -8,7 +8,7 @@ public class ApplicationTest extends FunctionalTest {
 
     @Test
     public void testThatIndexPageWorks() {
-        Response response = GET("/products");
+        Response response = GET("/");
         assertIsOk(response);
         assertContentType("text/html", response);
         assertCharset(play.Play.defaultWebEncoding, response);
