fad5e7b7da815f335bca3f77f895a8ed4cce5a83
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2014-09-23 14:17:11 +0100

    Add logging to tests to see why it's failing on Jenkins box.

--- test/FlowPageAccessTest.java
+++ test/FlowPageAccessTest.java
@@ -14,6 +14,7 @@ public class FlowPageAccessTest extends FunctionalTest {
     @Before
     public void loadFixtures() {
         Http.Response response = GET("/fixtures");
+        System.out.println("Loaded fixtures: " + response.status + " - " + response.toString());
     }
     
     @Test
@@ -27,16 +28,19 @@ public class FlowPageAccessTest extends FunctionalTest {
 
     private void checkCorrectPagesCanBeAccessedForFlow(String flowType, Long listingId) {
         for (Long pageId : ServiceSubmissionJourneyFlows.getFlow(flowType)){
+            System.out.println(String.format("Testing page works: /page/%d/%d", pageId, listingId)); 
             testThatPageWorks(String.format("/page/%d/%d", pageId, listingId));
         }
 
         for (Long pageId : pagesNotInFlow(flowType)){
+            System.out.println(String.format("Testing page doesn't work: /page/%d/%d", pageId, listingId));
             testThatPageNotInFlowDoesNotWork(String.format("/page/%d/%d", pageId, listingId));
         }
     }
     
     private void testThatPageWorks(String url) {
         Http.Response response = GET(url);
+        System.out.println("Response: " + response.status);
         assertIsOk(response);
         assertContentType("text/html", response);
         assertCharset(play.Play.defaultWebEncoding, response);
@@ -44,6 +48,7 @@ public class FlowPageAccessTest extends FunctionalTest {
 
     private void testThatPageNotInFlowDoesNotWork(String url) {
         Http.Response response = GET(url);
+        System.out.println("Response: " + response.status);
         assertIsNotFound(response);
     }
     
