89bc8bf621f32c2501c1e641b6058a2c843dd20e
brendanshort <brendan.short@digital.cabinet-office.gov.uk>
2014-09-25 14:25:41 +0100

    Finished page controllers 22 to 25

--- app/controllers/Page25.java
+++ app/controllers/Page25.java
@@ -4,29 +4,32 @@ import models.Listing;
 import models.Page;
 import play.mvc.Controller;
 
-// Not a real Controller - just a template that can serve as a skeleton for page controllers 
-
 public class Page25 extends Controller {
 
-    private static final Long PAGE_ID = -1l;
+    private static final Long PAGE_ID = 25l;
 
-    // TODO: This method will be very similar for all pages except for field-specific validation methods - how to factor out?
-    public static void savePage(Long listingId /* question responses passed in e.g. String[] p1_q1 */) {
+    public static void savePage(Long listingId, String p25q1, String p25q2, String p25q3, String p25q4) {
 
         Listing listing = Listing.getByListingId(listingId);
         
-        // TODO: Validate all fields on this page requiring validation
+        // Validate all fields on this page requiring validation
+        validation.required(p25q1).message("p25q1 : null");
+        validation.required(p25q2).message("p25q2 : null");
+        validation.required(p25q3).message("p25q3 : null");
+        validation.required(p25q4).message("p25q4 : null");
 
         if(validation.hasErrors()) {
             flash.error("%s", validation.errors());
             redirect(String.format("/page/%d/%d", PAGE_ID, listing.id));
         }
-        
-        int index = listing.pageSequence.indexOf(PAGE_ID);
-        //TODO: Save the form data as a Page into the correct page index
+
+        // Save the form data as a Page into the correct page index
         Page page = new Page(listingId, PAGE_ID);
-        // page.responses.put("p1_q1", Arrays.asList(p1_q1).toString());
-        // ...etc. for all questions on page
+        page.responses.put("p25q1", p25q1);
+        page.responses.put("p25q2", p25q2);
+        page.responses.put("p25q3", p25q3);
+        page.responses.put("p25q4", p25q4);
+
         page.insert();
         listing.addResponsePage(page, PAGE_ID);
         redirect(listing.nextPageUrl(PAGE_ID, listing.id));
