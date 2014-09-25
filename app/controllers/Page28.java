15c7da885f30650c6310d7ee333c6bbad10fbf86
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2014-09-25 14:23:58 +0100

    First shot controllers for security questions.

--- app/controllers/Page28.java
+++ app/controllers/Page28.java
@@ -4,29 +4,22 @@ import models.Listing;
 import models.Page;
 import play.mvc.Controller;
 
-// Not a real Controller - just a template that can serve as a skeleton for page controllers 
-
 public class Page28 extends Controller {
 
-    private static final Long PAGE_ID = -1l;
+    private static final Long PAGE_ID = 28l;
 
-    // TODO: This method will be very similar for all pages except for field-specific validation methods - how to factor out?
-    public static void savePage(Long listingId /* question responses passed in e.g. String[] p1_q1 */) {
+    public static void savePage(Long listingId, String p28q1) {
 
         Listing listing = Listing.getByListingId(listingId);
-        
-        // TODO: Validate all fields on this page requiring validation
 
+        validation.required(p28q1).message("p28q1:null");
         if(validation.hasErrors()) {
             flash.error("%s", validation.errors());
             redirect(String.format("/page/%d/%d", PAGE_ID, listing.id));
         }
         
-        int index = listing.pageSequence.indexOf(PAGE_ID);
-        //TODO: Save the form data as a Page into the correct page index
         Page page = new Page(listingId, PAGE_ID);
-        // page.responses.put("p1_q1", Arrays.asList(p1_q1).toString());
-        // ...etc. for all questions on page
+        page.responses.put("p28q1", p28q1);
         page.insert();
         listing.addResponsePage(page, PAGE_ID);
         redirect(listing.nextPageUrl(PAGE_ID, listing.id));
