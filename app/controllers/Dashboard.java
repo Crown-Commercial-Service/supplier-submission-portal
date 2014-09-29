a7807ff5a039db2f34d2d51e9792026666048d7e
brendanshort <brendan.short@digital.cabinet-office.gov.uk>
2014-09-29 11:46:46 +0100

    Changing dashboard to use siena query...

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -2,7 +2,6 @@ package controllers;
 
 import models.Listing;
 import play.mvc.Controller;
-import siena.*;
 import uk.gov.gds.dm.DevUtils;
 
 import java.util.List;
@@ -11,7 +10,7 @@ public class Dashboard extends Controller {
 
     public static void home() {
         String supplierId = DevUtils.randomSupplierId();
-        List<Listing> listings = Listing.allBySupplierId(supplierId);
+        List<Listing> listings = Listing.all(Listing.class).filter("supplierId", supplierId).fetch();
         render(listings, supplierId);
     }
 }
