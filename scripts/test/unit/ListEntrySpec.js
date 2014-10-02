66ce45ff7f1a1ef6dae638ef1e58e153e2e884dd
Tom Byers <tombaromba@gmail.com>
2014-10-02 09:57:59 +0100

    Stub out initial tests

---
+++ scripts/test/unit/ListEntrySpec.js
@@ -0,0 +1,43 @@
+describe("ListEntryField", function () {
+  var entryFieldHTML = '<label for="{{{id}}}" class="text-box-number-label">' + 
+                      '<span class="hidden">Fieldset legend number </span>{{number}}.' +
+                    '</label>';
+
+  describe("When called", function () {
+    it("Should remove all the fields except the first 2 if no values are present on page load", function () {
+      
+    });
+
+    it("Should remove all the fields except those with values if some have values on page load", function () {
+
+    });
+
+    it("Should add 'remove' buttons to all fields except the first", function () {
+
+    });
+
+    it("Should add an 'add feature' button to the bottom of the list", function () {
+
+    });
+  });
+
+  describe("When the 'remove' button is clicked", function () {
+    it("Should remove the associated field", function () {
+
+    });
+
+    it("Should leave the list with the right numbers", function () {
+
+    });
+  }); 
+
+  describe("When the 'add feature' button is clicked", function () {
+    it("Should add a new field", function () {
+
+    });
+
+    it("Should update the number of fields users are allowed to enter to consider those already entered", function () {
+
+    });
+  });
+});
