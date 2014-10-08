1c6bcc90b561f64e1b08a27fa7cd8bad6b29dd7e
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2014-10-08 12:24:12 +0100

    Add log messages to test
    Test running fine locally with auto-test but failing on Jenkins.  This extra log output should help us diagnose the cause.

--- test/functional/FlowPageAccessTest.java
+++ test/functional/FlowPageAccessTest.java
@@ -1,6 +1,7 @@
 package functional;
 
 import org.junit.*;
+import play.Logger;
 import play.mvc.Http;
 import play.test.FunctionalTest;
 import uk.gov.gds.dm.FixtureDataSetup;
@@ -15,8 +16,9 @@ public class FlowPageAccessTest extends FunctionalTest {
     
     @Before
     public void loadFixtures() {
+        Logger.info("Loading fixtures...");
         Http.Response response = GET("/fixtures");
-        System.out.println("Loaded fixtures: " + response.status + " - " + response.toString());
+        Logger.info("Loaded fixtures: " + response.status + " - " + response.toString());
     }
 
     @Test
@@ -30,19 +32,19 @@ public class FlowPageAccessTest extends FunctionalTest {
 
     private void checkCorrectPagesCanBeAccessedForFlow(String flowType, Long listingId) {
         for (Long pageId : ServiceSubmissionJourneyFlows.getFlow(flowType)){
-            System.out.println(String.format("   Testing page works [%s]: /page/%d/%d", flowType, pageId, listingId)); 
+            Logger.info(String.format("   Testing page works [%s]: /page/%d/%d", flowType, pageId, listingId)); 
             testThatPageWorks(String.format("/page/%d/%d", pageId, listingId));
         }
 
         for (Long pageId : pagesNotInFlow(flowType)){
-            System.out.println(String.format("   Testing page doesn't work [%s]: /page/%d/%d", flowType, pageId, listingId));
+            Logger.info(String.format("   Testing page doesn't work [%s]: /page/%d/%d", flowType, pageId, listingId));
             testThatPageNotInFlowDoesNotWork(String.format("/page/%d/%d", pageId, listingId));
         }
     }
     
     private void testThatPageWorks(String url) {
         Http.Response response = GET(url);
-        System.out.println("     Response: " + response.status);
+        Logger.info("     Response: " + response.status);
         assertIsOk(response);
         assertContentType("text/html", response);
         assertCharset(play.Play.defaultWebEncoding, response);
@@ -50,7 +52,7 @@ public class FlowPageAccessTest extends FunctionalTest {
 
     private void testThatPageNotInFlowDoesNotWork(String url) {
         Http.Response response = GET(url);
-        System.out.println("     Response: " + response.status);
+        Logger.info("     Response: " + response.status);
         assertIsNotFound(response);
     }
     
