77780bde2482a307e6f16336570ae94cafd1d1a7
brendanshort <brendan.short@digital.cabinet-office.gov.uk>
2014-10-09 12:40:57 +0100

    Updated URLs to change depending on the environment

--- app/uk/gov/gds/dm/URLTools.java
+++ app/uk/gov/gds/dm/URLTools.java
@@ -6,11 +6,19 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class URLTools {
+
     public static String getDigitalMarketplaceURL(){
         if(Play.mode == Play.Mode.DEV){
             return getLocalDigitalMarketplaceURL();
         } else {
-            return getProductionDigitalMarketplaceURL();
+            String appName = Play.configuration.getProperty("application.name");
+            if(appName.equals("ssp-qa")){
+                return getQADigitalMarketplaceURL();
+            } else if (appName.equals("ssp-staging")){
+                return getStagingDigitalMarketplaceURL();
+            } else {
+                return getProductionDigitalMarketplaceURL();
+            }
         }
     }
 
@@ -36,6 +44,26 @@ public class URLTools {
         }
     }
 
+    private static String getStagingDigitalMarketplaceURL(){
+        String[] localDigitalMarketplaceURLs = new String[] {"https://stage.digitalmarketplace.service.gov.uk/", "https://stage.digitalmarketplace.service.gov.uk/digitalmarketplace/"};
+
+        if(isWorkingURL(localDigitalMarketplaceURLs[0])){
+            return localDigitalMarketplaceURLs[0];
+        } else {
+            return localDigitalMarketplaceURLs[1];
+        }
+    }
+
+    private static String getQADigitalMarketplaceURL(){
+        String[] localDigitalMarketplaceURLs = new String[] {"http://qa.digitalmarketplace.service.gov.uk:8080/", "http://qa.digitalmarketplace.service.gov.uk:8080/digitalmarketplace/"};
+
+        if(isWorkingURL(localDigitalMarketplaceURLs[0])){
+            return localDigitalMarketplaceURLs[0];
+        } else {
+            return localDigitalMarketplaceURLs[1];
+        }
+    }
+
     private static boolean isWorkingURL(String url){
         try {
             final URL testURL = new URL(url);
@@ -47,7 +75,6 @@ public class URLTools {
             final int code = huc.getResponseCode();
             return code != 404;
         } catch (Exception e){
-            System.out.println("There was an error testing the DM URL: " + e.getMessage());
             return false;
         }
     }
