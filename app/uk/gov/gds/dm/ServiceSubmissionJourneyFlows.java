f1a81d767504bc8f43f796fab66c3e52977dbab2
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2014-10-09 10:44:21 +0100

    Add extra pages to relevant journey flows
    New security question pages need to be added to the flow so that they are displayed to suppliers in the relevant lots.

--- app/uk/gov/gds/dm/ServiceSubmissionJourneyFlows.java
+++ app/uk/gov/gds/dm/ServiceSubmissionJourneyFlows.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class ServiceSubmissionJourneyFlows {
 
-    private final static List<Long> iaasFlow = Arrays.asList(1l, 4l, 5l, 6l, 7l, 8l, 9l, 10l, 11l, 12l, 13l, 14l, 15l, 17l, 18l, 19l, 20l, 22l, 23l, 24l, 25l, 26l, 27l, 28l, 29l, 30l, 31l, 32l, 33l, 34l, 35l, 36l);
-    private final static List<Long> saasFlow = Arrays.asList(2l, 4l, 5l, 6l, 7l, 8l, 9l, 10l, 11l, 12l, 13l, 14l, 15l, 16l, 17l, 18l, 19l, 20l, 21l, 22l, 23l, 24l, 25l, 26l, 27l, 28l, 29l, 30l, 31l, 32l, 33l, 34l, 35l, 36l);
-    private final static List<Long> paasFlow = Arrays.asList(4l, 5l, 6l, 7l, 8l, 9l, 10l, 11l, 12l, 13l, 14l, 15l, 17l, 18l, 19l, 20l, 22l, 23l, 24l, 25l, 26l, 27l, 28l, 29l, 30l, 31l, 32l, 33l, 34l, 35l, 36l);
+    private final static List<Long> iaasFlow = Arrays.asList(1l, 4l, 5l, 6l, 7l, 8l, 9l, 10l, 11l, 12l, 13l, 14l, 15l, 17l, 18l, 19l, 20l, 22l, 23l, 24l, 25l, 26l, 27l, 28l, 29l, 30l, 31l, 32l, 33l, 34l, 35l, 36l, 37l, 38l, 39l, 40l);
+    private final static List<Long> saasFlow = Arrays.asList(2l, 4l, 5l, 6l, 7l, 8l, 9l, 10l, 11l, 12l, 13l, 14l, 15l, 16l, 17l, 18l, 19l, 20l, 21l, 22l, 23l, 24l, 25l, 26l, 27l, 28l, 29l, 30l, 31l, 32l, 33l, 34l, 35l, 36l, 37l, 38l, 39l, 40l);
+    private final static List<Long> paasFlow = Arrays.asList(4l, 5l, 6l, 7l, 8l, 9l, 10l, 11l, 12l, 13l, 14l, 15l, 17l, 18l, 19l, 20l, 22l, 23l, 24l, 25l, 26l, 27l, 28l, 29l, 30l, 31l, 32l, 33l, 34l, 35l, 36l, 37l, 38l, 39l, 40l);
     private final static List<Long> scsFlow = Arrays.asList(3l, 4l, 5l, 6l, 7l, 8l, 10l, 20l, 24l, 28l, 30l);
 
     public static List<Long> getFlow(String lot) throws IllegalArgumentException {
