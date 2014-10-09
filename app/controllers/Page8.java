a712966ceb2cddcc08877811fb2c10037f394065
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2014-10-09 12:49:39 +0100

    Add logic to controller to handle new pricing format
    There is a new format for the Pricing question. This update to the controller saves the new fields properly.

--- app/controllers/Page8.java
+++ app/controllers/Page8.java
@@ -15,15 +15,15 @@ public class Page8 extends AuthenticatingController {
 
     private static final Long PAGE_ID = 8l;
 
-    public static void savePage(Long listingId, String[][] p8q1, String p8q2, String p8q3, String p8q4, String p8q5, Upload p8q6, Upload p8q7) {
+    public static void savePage(Long listingId, Double p8q1MinPrice, Double p8q1MaxPrice, String p8q1Unit, String p8q1Interval,
+                                String p8q2, String p8q3, String p8q4, String p8q5, Upload p8q6, Upload p8q7) {
 
         Listing listing = Listing.getByListingId(listingId);
 
-        if(p8q1 != null) {
-            // TODO: Validate arrays
-        } else {
-            // validation.required(p8q1); // TODO: pricing matrix
-        }
+        validation.required(p8q1MinPrice).key("p8q1MinPrice");
+        validation.required(p8q1Unit).key("p8q1Unit");
+        validation.maxSize(p8q1Unit, 25);
+        validation.maxSize(p8q1Interval, 25);
         validation.required(p8q2).key("p8q2");
         validation.maxSize(p8q2, 10);
         validation.required(p8q3).key("p8q3");
@@ -68,7 +68,10 @@ public class Page8 extends AuthenticatingController {
 
         Gson gson = new Gson();
         Page page = new Page(listingId, PAGE_ID);
-        page.responses.put("p8q1", gson.toJson(p8q1));
+        page.responses.put("p8q1MinPrice", Double.toString(p8q1MinPrice));
+        page.responses.put("p8q1MaxPrice", Double.toString(p8q1MaxPrice));
+        page.responses.put("p8q1Unit", p8q1Unit);
+        page.responses.put("p8q1Interval", p8q1Interval);
         page.responses.put("p8q2", p8q2);
         page.responses.put("p8q3", p8q3);
         page.responses.put("p8q4", p8q4);
