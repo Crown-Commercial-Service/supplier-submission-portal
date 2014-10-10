457c10aa7b544f2dbb2932ffe6b6b881f82a351d
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2014-10-10 12:28:11 +0100

    Pass invalid responses through to display
    When there are validation errors with submitted answers those answers need to be passed through to the page display controller so that that can be used to re-populate form fields and users' work is not lost.

--- app/controllers/QuestionPageDisplay.java
+++ app/controllers/QuestionPageDisplay.java
@@ -1,8 +1,13 @@
 package controllers;
 
+import com.google.gson.Gson;
 import models.Listing;
 import models.Page;
+import play.Logger;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.*;
 
 public class QuestionPageDisplay extends AuthenticatingController {
 
@@ -29,11 +34,59 @@ public class QuestionPageDisplay extends AuthenticatingController {
         renderArgs.put("pageTotal", Integer.toString(listing.pageSequence.size()));
         renderArgs.put("listingId", listingId);
 
-        Page page = listing.completedPages.get(index);
-        if (page!= null) {
-            renderArgs.put("oldValues", page.responses);
+        
+        
+        if (flash.get("body") != null) {
+            try {
+                renderArgs.put("oldValues", getInvalidAnswers());
+            } catch (Exception ex) {
+                Logger.error("Error reading in previously entered responses", ex);
+            }
+        } else {
+            Page page = listing.completedPages.get(index);
+            if (page!= null) {
+                renderArgs.put("oldValues", page.responses);
+            }
         }
+        
         renderArgs.put("serviceName", listing.title);
         renderTemplate(String.format("QuestionPages/%d.html", pageId));
+    }
+
+    private static Map<String, String> getInvalidAnswers() throws UnsupportedEncodingException {
+        return flatten(splitParams(flash.get("body")));
+    }
+    
+    // Code based on answer given by user Pr0gr4mm3r at:
+    // http://stackoverflow.com/questions/13592236/parse-the-uri-string-into-name-value-collection-in-java
+    private static Map<String, List<String>> splitParams(String paramString) throws UnsupportedEncodingException {
+        final Map<String, List<String>> query_pairs = new LinkedHashMap();
+        final String[] pairs = paramString.split("&");
+        for (String pair : pairs) {
+            final int idx = pair.indexOf("=");
+            final String key = idx > 0 ? URLDecoder.decode(pair.substring(0, idx), "UTF-8") : pair;
+            if (!query_pairs.containsKey(key)) {
+                query_pairs.put(key, new LinkedList<String>());
+            }
+            final String value = idx > 0 && pair.length() > idx + 1 ? URLDecoder.decode(pair.substring(idx + 1), "UTF-8") : null;
+            query_pairs.get(key).add(value);
+        }
+        return query_pairs;
+    }
+    
+    private static Map<String, String> flatten(Map<String, List<String>> map) {
+        Map<String, String> toReturn = new HashMap();
+        Gson gson = new Gson();
+        for(String key : map.keySet()) {
+            if (map.get(key).isEmpty()){
+                toReturn.put(key, null);
+            } else if (map.get(key).size() == 1) {
+                toReturn.put(key, map.get(key).get(0));
+            }
+            else if (map.get(key).size() == 1) {
+                toReturn.put(key, gson.toJson(map.get(key)));
+            }
+        }
+        return toReturn;
     }
 }
