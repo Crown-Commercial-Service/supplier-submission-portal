73465073c9e1754a17e30c52b4446d5c0836da23
Chris Hill-Scott <me@quis.cc>
2014-10-13 17:42:51 +0100

    Fix problem Jenkins had with spaces in job names

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -10,11 +10,12 @@ npm install
 # Generate the content as a properties file
 node run_grunt.js content
 
+# Run Jasmine tests of client side code
+node run_grunt.js dev
+node run_grunt.js test
+
 # Generate assets into public
 node run_grunt.js production
-
-# Run Jasmine tests of client side code
-node run_grunt.js test
 
 # Run unit and Selenium tests within play
 ./scripts/updateEnvironment.sh ssp-preview ${ENCRYPTION_KEY} false
@@ -26,13 +27,13 @@ play auto-test
 ./scripts/updateEnvironment.sh ssp-staging ${ENCRYPTION_KEY} false
 play clean
 play deps
-play war -o /data/supplier-submission/last-successful/staging-${BUILD_TAG}.war
+play war -o /data/supplier-submission/last-successful/submissions-staging-${BUILD_NUMBER}.war
 
 # Build artefact for live
 ./scripts/updateEnvironment.sh ssp-live ${ENCRYPTION_KEY} true
 play clean
 play deps
-play war -o /data/supplier-submission/last-successful/live-${BUILD_TAG}.war
+play war -o /data/supplier-submission/last-successful/submissions-live-${BUILD_NUMBER}.war
 
-git tag ${BUILD_TAG} -m "To deploy this build, use the following build number in Jenkins: ${BUILD_NUMBER}"
+git tag -a jenkins-build-${BUILD_NUMBER} -m "To deploy this build, use the following build number in Jenkins: ${BUILD_NUMBER}"
 git push --tags
