5c099c6caa0c823b8e44104edd93299935c561fd
George McIntosh <george.mcintosh@digital.cabinet-office.gov.uk>
2014-10-15 12:23:49 +0100

    [GM] changed optional questions

--- app/uk/gov/gds/dm/ServiceSubmissionJourneyFlows.java
+++ app/uk/gov/gds/dm/ServiceSubmissionJourneyFlows.java
@@ -9,7 +9,7 @@ public class ServiceSubmissionJourneyFlows {
     private final static List<Long> saasFlow = Arrays.asList(2l, 4l, 5l, 6l, 7l, 8l, 9l, 10l, 11l, 12l, 13l, 14l, 15l, 16l, 17l, 18l, 19l, 20l, 21l, 22l, 23l, 24l, 25l, 26l, 27l, 28l, 29l, 30l, 31l, 32l, 33l, 34l, 35l, 36l, 37l, 38l, 39l, 40l);
     private final static List<Long> paasFlow = Arrays.asList(4l, 5l, 6l, 7l, 8l, 9l, 10l, 11l, 12l, 13l, 14l, 15l, 17l, 18l, 19l, 20l, 22l, 23l, 24l, 25l, 26l, 27l, 28l, 29l, 30l, 31l, 32l, 33l, 34l, 35l, 36l, 37l, 38l, 39l, 40l);
     private final static List<Long> scsFlow = Arrays.asList(3l, 4l, 5l, 6l, 7l, 8l, 10l, 20l, 24l, 30l, 31l, 33l);
-    private final static List<String> optionalQuestions = Arrays.asList("p4q1", "p8q4", "p8q5");
+    private final static List<String> optionalQuestions = Arrays.asList("p8q7", "p16q1", "p17q2", "p20q1", "p21q1");
 
     public static List<Long> getFlow(String lot) throws IllegalArgumentException {
         if (lot.equals("IaaS")) {
