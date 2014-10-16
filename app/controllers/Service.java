ecc560dd6cfb56dc6cd887189f84769113f55c75
brendanshort <brendan.short@digital.cabinet-office.gov.uk>
2014-10-16 10:14:14 +0100

    Added new controller to handle rendering the summary page with a delete message

--- app/controllers/Service.java
+++ app/controllers/Service.java
@@ -100,9 +100,30 @@ public class Service extends AuthenticatingController {
         redirect("/");
     }
 
-    public static void showDeletePage(Long listingId){
+    public static void showDeletePage(Long listingId, Boolean showDeleteMessage){
         Listing listing = Listing.getByListingId(listingId);
-        renderTemplate(String.format("Service/delete.html"), listing);
+        List<Long> flow = ServiceSubmissionJourneyFlows.getFlow(listing.lot);
+        List<String> optionalQuestions = ServiceSubmissionJourneyFlows.getOptionalQuestions();
+
+        Map<String, String> allAnswers = new HashMap<String, String>();
+        for(Page p : listing.completedPages){
+            if(p.responses != null){
+                allAnswers.putAll(p.responses);
+            }
+        }
+
+        System.out.println(allAnswers);
+
+        renderArgs.put("content", Fixtures.getContentProperties());
+        renderArgs.put("service_id", listingId);
+        renderArgs.put("listing", listing);
+        renderArgs.put("flow", flow);
+        renderArgs.put("maxPossibleNumberOfQuestions", 20);
+        renderArgs.put("optionalQuestions", optionalQuestions);
+        renderArgs.put("pageIndex", 0);
+        renderArgs.put("storedValues", allAnswers);
+        renderArgs.put("showDeleteMessage", showDeleteMessage);
+        renderTemplate("Service/summaryPage.html", listingId);
     }
 
     public static void delete(Long listingId){
