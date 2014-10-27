946acc3a35fdd462b7252fe323dc83ab005ef990
minglis <martyn.inglis@digital.cabinet-office.gov.uk>
2014-10-27 15:51:56 +0000

    Added generateCron groovy script to make the cron xml file to enable nightly backups. Uses google cloud buckets to store all entities from the database.

---
+++ scripts/generateCron.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/env groovy
+import groovy.xml.MarkupBuilder
+
+def writer = new FileWriter('./war/WEB-INF/cron.xml')
+
+def xml = new MarkupBuilder(writer)
+writer.append('<?xml version="1.0" encoding="UTF-8"?>\n')
+xml.'cronentries' {
+    'cron' {
+        'description'('G6 Submissions Backups')
+        'url'('/_ah/datastore_admin/backup.create?name=live&amp;amp;kind=Document&amp;kind=Page&amp;kind=listing&amp;filesystem=gs&amp;gs_bucket_name=ssp-live-backups')
+        'schedule'('every day 00:00')
+        'target'('ah-builtin-python-bundle')
+    }
+}
+
+writer.flush()
