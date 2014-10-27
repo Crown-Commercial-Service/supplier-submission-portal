311d8d8ded9062f86053250bd9f2ebc6a98e2704
brendanshort <brendan.short@digital.cabinet-office.gov.uk>
2014-10-27 17:05:23 +0000

    Remove debugging stuff

--- app/controllers/AuthenticatingController.java
+++ app/controllers/AuthenticatingController.java
@@ -59,8 +59,6 @@ public abstract class AuthenticatingController extends Controller {
         } else {
             populateMapWithSSOCookieData(gdmSsoCookie);
             CookieUtils.updateSSOCookieWithCurrentTimestamp(supplierDetailsFromCookie);
-
-            Logger.info("Logged in, cookie is secure? (" + gdmSsoCookie.secure + ")");
         }
     }
 
