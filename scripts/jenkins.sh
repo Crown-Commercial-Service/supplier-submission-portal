c6aeddf13d049100a5c82e21e4f3c98d5409f466
minglis <martyn.inglis@digital.cabinet-office.gov.uk>
2014-10-28 09:13:49 +0000

    Added the cron generate groovy script to the jenkins script to be made on the production artefact only. Generates the cron.xml to enable backups.

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -31,6 +31,8 @@ play war -o /data/supplier-submission/last-successful/submissions-staging-${BUIL
 
 # Build artefact for live
 ./scripts/updateEnvironment.sh ssp-live ${ENCRYPTION_KEY} true gds-g6-submission-bucket-live
+./scripts/generateWebDescriptor.groovy
+
 ## use production web.xml
 mv ./war/WEB-INF/web.xml ./war/WEB-INF/web-not-production.xml
 mv ./war/WEB-INF/web-production.xml ./war/WEB-INF/web.xml
