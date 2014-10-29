c93d76c622c04b6e77387e08d447dd93122f2ced
brendanshort <brendan.short@digital.cabinet-office.gov.uk>
2014-10-29 15:02:03 +0000

    RM cron jobs

--- scripts/generateCron.groovy
+++ scripts/generateCron.groovy
@@ -6,17 +6,7 @@ def writer = new FileWriter('./war/WEB-INF/cron.xml')
 def xml = new MarkupBuilder(writer)
 writer.append('<?xml version="1.0" encoding="UTF-8"?>\n')
 xml.'cronentries' {
-    'cron' {
-        'description'('G6 Submissions Backups')
-        'url'('/_ah/datastore_admin/backup.create?name=live&amp;amp;kind=Document&amp;kind=Page&amp;kind=listing&amp;filesystem=gs&amp;gs_bucket_name=ssp-live-backups')
-        'schedule'('every day 00:00')
-        'target'('ah-builtin-python-bundle')
-    }
-    'cron' {
-        'description'('Export of all completed listings to S3')
-        'url'('/cron/export')
-        'schedule'('every 2 minutes')
-    }
+
 }
 
 writer.flush()
