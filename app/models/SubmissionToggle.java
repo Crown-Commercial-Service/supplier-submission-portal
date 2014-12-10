de5317db9b7764d3fb965ab83c31c4887e290143
brendanshort <brendan.short@digital.cabinet-office.gov.uk>
2014-12-10 17:03:24 +0000

    Added submissions toggling via DB

---
+++ app/models/SubmissionToggle.java
@@ -0,0 +1,19 @@
+package models;
+
+import siena.*;
+
+public class SubmissionToggle extends Model {
+    // For GAE :
+    // 1. @Id annotated field corresponding to the primary key must be Long type
+    // 2. @Id annotated field corresponding to the primary key must be called "id"
+    @Id(Generator.AUTO_INCREMENT)
+    public Long id;
+
+    @Column("enabled")
+    @NotNull
+    public long enabled;
+
+    public SubmissionToggle() {
+        this.enabled = 0;
+    }
+}
