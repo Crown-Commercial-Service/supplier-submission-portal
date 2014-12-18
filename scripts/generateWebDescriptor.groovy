69a48ac571eeec1d58984626c28f03ce7124ff37
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2014-12-18 11:29:43 +0000

    [KK] Update version number
    I think it makes sense to update the version number for post-live code updates.

--- scripts/generateWebDescriptor.groovy
+++ scripts/generateWebDescriptor.groovy
@@ -16,7 +16,7 @@ def xml = new MarkupBuilder(writer)
 
 xml.'appengine-web-app'( xmlns: 'http://appengine.google.com/ns/1.0') {
     application(appName)
-    version('1-0-5')
+    version('1-0-6')
     'threadsafe' ('true')
     'ssl-enabled'(sslEnabled)
     'system-properties' {
